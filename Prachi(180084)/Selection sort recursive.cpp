b06385f1f0f584c6a1566d0d573d608a5d4fa237
Prachi0302 <60928617+Prachi0302@users.noreply.github.com>
2020-12-04 15:36:13 +0530

    Add files via upload

---
+++ Prachi(180084)/Selection sort recursive.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int minIndex(int a[], int i, int j)
+{
+if (i == j)
+return i;
+int k = minIndex(a, i + 1, j);
+return (a[i] < a[k])? i : k;
+}
+void recurSelectionSort(int a[], int n, int index = 0)
+{
+if (index == n)
+return;
+int k = minIndex(a, index, n-1);
+if (k != index)
+swap(a[k], a[index]);
+recurSelectionSort(a, n, index + 1);
+}
+int main()
+{
+int arr[] = {4, 2, 6, 3, 9, 0};
+int n = sizeof(arr)/sizeof(arr[0]);
+recurSelectionSort(arr, n);
+for (int i = 0; i<n ; i++)
+cout << arr[i] << " ";
+cout << endl;
+return 0;
+}
